9cb75e9b3d88080696925a48a8ad575dbcfcba08
Osmar Oliveira <osmar.oliveira@fieb.org.br>
2023-07-10 22:12:39 -0300

    minor change to better reading

--- rm_sim/__init__.py
+++ rm_sim/__init__.py
@@ -25,18 +25,20 @@ class RMSim:
                     print(f'{self.executing_task} executed for {self.sleeping_tasks[0].next_start - self.time} time units')
                     self.executing_task.execute(self.time, self.sleeping_tasks[0].next_start - self.time)
                     self.time += self.sleeping_tasks[0].next_start - self.time
-                    self.sleeping_tasks[0].start_task()
-                    bisect.insort(self.waiting_tasks, self.sleeping_tasks.pop(0), key=lambda x: -1 * x.priority)
+                    self.wake_up_task()
                     bisect.insort(self.waiting_tasks, self.executing_task, key=lambda x: -1 * x.priority)
                     return
                 else:
-                    self.sleeping_tasks[0].start_task()
-                    bisect.insort(self.waiting_tasks, self.sleeping_tasks.pop(0), key=lambda x: -1 * x.priority)
+                    self.wake_up_task()
         print(f'{self.executing_task} executed for {self.executing_task.remaining_time} time units')
         executed = self.executing_task.remaining_time
         self.executing_task.execute(self.time, self.executing_task.remaining_time)
         bisect.insort(self.sleeping_tasks, self.executing_task, key=lambda x: -1 * x.priority)
         self.time += executed
+
+    def wake_up_task(self) -> None:
+        self.sleeping_tasks[0].start_task()
+        bisect.insort(self.waiting_tasks, self.sleeping_tasks.pop(0), key=lambda x: -1 * x.priority)
 
     def insert_idle_time(self, time, idle_time):
         self.idle_time_chart.append([time, idle_time])
