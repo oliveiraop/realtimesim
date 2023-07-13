5d642d338956b60188f97aa269358225e27fda17
Osmar Oliveira <osmar.oliveira@fieb.org.br>
2023-07-13 15:29:08 -0300

    generate fig on example

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from rm_sim import RMSim
 from common import Task
+import gantt
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
@@ -10,5 +12,10 @@ if __name__ == '__main__':
     sim = RMSim(tasks)
     sim.simulate(50)
 
+    execution_charts = []
     for task in tasks:
+        execution_charts.append(task.execution_chart)
         print(task.execution_chart)
+
+    fig = gantt.plot_grant(execution_charts, [])
+    plt.savefig("gantt1.png")
