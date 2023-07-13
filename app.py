9283acd448291c8549f26261aec04dde5664e47d
Gabriel Andrade <gabriel.andrad4@gmail.com>
2023-07-13 15:28:03 -0300

    link main to RMSIM

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     tasks.append(Task(1, 20, 10, 3))
     tasks.append(Task(2, 15, 5, 4))
     tasks.append(Task(3, 5, 2, 5))
-    sim = RMSim(tasks)
+    sim = RMSim(tasks)  
     sim.simulate(50)
 
     for task in tasks:
--- common/__init__.py
+++ common/__init__.py
@@ -11,19 +11,24 @@ class Task:
         self.executed = False
         self.priority = priority
         self.execution_chart = []
+        self.execution_endings = []
 
     def __repr__(self) -> str:
         return f"Task{self.number}"
     
     def execute(self, start_time, execution_time: int) -> bool:
         self.remaining_time -= execution_time
-        if self.remaining_time == 0:
+        if self.remaining_time <= 0:
+            if start_time + execution_time > self.next_deadline:
+                print(f"task{self.number} failed to execute at {start_time + execution_time}, deadline was {self.next_deadline}")
             self.executed = True
+            self.execution_endings.append(start_time + execution_time)
         self.execution_gantt(start_time, execution_time)
     
     def start_task(self) -> None:
         self.next_start += self.period
         self.next_deadline += self.period
+        print(f"next deadline for task{self.number} is {self.next_deadline}")
         self.remaining_time = self.execution_time
         self.executed = False
 
